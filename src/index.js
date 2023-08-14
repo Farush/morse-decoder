a45c152f4937a9c3671f4d05222160ce59a2dd85
Farkhad Sibgatullin <farusha.q@gmail.com>
2023-08-15 02:16:51 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -37,10 +37,43 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const toDigitalLettersArray = (expr) => {
+    let digitalLettersArray = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        digitalLettersArray.push(expr.slice(i, i + 10));
+    }
+    return digitalLettersArray;
+};
+
+
+const letterDecode = (digitalLetter) => {
+    morseLetter = ''
+    for (let i = 0; i < 10; i += 2) {
+        if (digitalLetter.slice(i, i + 2) === '10') morseLetter += '.';
+        if (digitalLetter.slice(i, i + 2) === '11') morseLetter += '-';
+    }
+    return morseLetter;
+}
+
+const toHumanPhrase = (digitalLettersArray) => {
+    humanPhrase = '';
+    for (let i = 0; i < digitalLettersArray.length; i++) {
+        if (digitalLettersArray[i] === '**********') {
+            humanPhrase = humanPhrase + ' '
+        }
+        else { 
+            humanPhrase = humanPhrase + MORSE_TABLE[letterDecode(digitalLettersArray[i])] 
+        }
+    }
+return humanPhrase
+
+}
+
 function decode(expr) {
-    // write your solution here
+    return toHumanPhrase(toDigitalLettersArray(expr))
 }
 
 module.exports = {
     decode
 }
+
